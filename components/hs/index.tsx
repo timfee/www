00777a8d969ae2b2cae1459925b82e86ef91262f
Tim Feeley <tim@timfeeley.com>
2023-04-03 16:06:59 -0700

    case

---
+++ components/hs/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+import type { HTMLProps } from "react"
+
+import source from "./image.png"
+
+export default function Headshot({
+  height: _height,
+  width: _width,
+  className,
+}: HTMLProps<HTMLImageElement>) {
+  const ASPECT_RATIO = 640 / 647
+
+  let height = _height ? Number.parseInt(_height.toString()) : undefined
+  let width = _width ? Number.parseInt(_width.toString()) : undefined
+
+  if (height) {
+    width = Math.round(height * ASPECT_RATIO)
+  } else if (width) {
+    height = Math.round(width / ASPECT_RATIO)
+  } else {
+    throw new Error("Headshot must have either a height or width")
+  }
+
+  return (
+    <figure className={className} style={{ width, height }}>
+      <Image
+        priority
+        height={height}
+        width={width}
+        src={source}
+        alt="Tim Feeley"
+      />
+    </figure>
+  )
+}
---
+++ components/lgs/index.ts
@@ -0,0 +1,3 @@
+export { default as GithubSvg } from "./github"
+export { default as LinkedInSvg } from "./linkedin"
+export { default as TwitterSvg } from "./twitter"
